3845fdf0f99d79df7a02160979119b41ecc2e4ba
Em Harrington <emharrington@MASTUDENT-Em-Harrington-C17GMB2CQ6L4.local>
2022-09-21 13:21:02 +0100

    diary entry

---
+++ lib/DiaryEntry.rb
@@ -0,0 +1,43 @@
+class DiaryEntry
+    def initialize(title, contents)
+      @title = title
+      @contents = contents
+      @furthest_word_read = 0
+    end
+  
+    def title
+      return @title
+    end
+  
+    def contents
+      return @contents
+    end
+  
+    def count_words
+      @contents.split(" ").length
+    end
+  
+    def reading_time(wpm)
+      fail "Reading time but be more than 0." unless wpm.positive?
+      return (count_words / wpm.to_f).ceil
+    end
+  
+    def reading_chunk(wpm, minutes)
+      words_we_can_read = wpm * minutes
+      start_from = @furthest_word_read
+      end_at = @furthest_word_read + words_we_can_read
+      word_list = words[start_from, end_at]
+      if end_at >= count_words
+        @furthest_word_read = 0
+      else
+        @furthest_word_read = end_at
+      end
+      return word_list.join(" ")
+    end
+
+    private
+
+    def words
+      return @contents.split(" ")
+    end
+  end
